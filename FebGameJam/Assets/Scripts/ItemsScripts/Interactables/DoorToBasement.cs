18b081d265826bf2d1719c720f11b7b0c534f03b
Anthony Ferraioli <amf7619@ad.rit.edu>
2020-02-22 18:38:19 -0500

    can leave baseent

---
+++ FebGameJam/Assets/Scripts/ItemsScripts/Interactables/DoorToBasement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorToBasement : InteractableItem
+{
+    // camera reference
+    [SerializeField] Camera mainCamera;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        // move camera to bathroom
+        mainCamera.transform.position = new Vector3(-160, -230, -100);
+    }
+}
