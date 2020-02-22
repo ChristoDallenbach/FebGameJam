00a7a94aa13a7f40067b751d796c9fff369e8999
Anthony Ferraioli <amf7619@ad.rit.edu>
2020-02-21 22:30:18 -0500

    grab

--- FebGameJam/Assets/Scripts/ItemsScripts/Grabables/GrabableItem.cs
+++ FebGameJam/Assets/Scripts/ItemsScripts/Grabables/GrabableItem.cs
@@ -10,10 +10,14 @@ public class GrabableItem : ItemScript
     // put item into empty inventory spot
     public override void Interact()
     {
+        Debug.Log("scoop");
         // getting the sprite for drawing in inventory
         objectTexture = gameObject.GetComponent<SpriteRenderer>().sprite;
 
         // get the inventory manager   
         GameObject.Find("InventoryManager").GetComponent<Inventory>().AddNewItem(this);
+
+        // destroy the gameobject
+        GameObject.Destroy(gameObject);
     }
 }
--- FebGameJam/Assets/Scripts/Utilities/Inventory.cs
+++ FebGameJam/Assets/Scripts/Utilities/Inventory.cs
@@ -16,6 +16,7 @@ public class Inventory : MonoBehaviour
 
     public void AddNewItem(GrabableItem newItem) // Adds items to the list
     {
+        Debug.Log("hooplah");
         items.Add(newItem);
     }
 
@@ -33,7 +34,7 @@ public class Inventory : MonoBehaviour
             GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
 
             // Display Item and other info
-            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 50, 50), items[zoomObject] + "");
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200, 200), items[zoomObject].objectTexture.texture);
             // Display text that describes the object maybe
 
             if(GUI.Button(new Rect(0, 0, 100, 100), "Back")) // Right click to un-zoom
@@ -46,7 +47,7 @@ public class Inventory : MonoBehaviour
 
             for (int i = 0; i < items.Count; i++) // Makes buttons that do something when clicked for items that currently exist
             {
-                if (GUI.Button(new Rect(Screen.width - 100, (i * (Screen.height / MAX_ITEMS)) + (Screen.height / (MAX_ITEMS * 2)) - 50, 100, 100), items[i] + ""))
+                if (GUI.Button(new Rect(Screen.width - 100, (i * (Screen.height / MAX_ITEMS)) + (Screen.height / (MAX_ITEMS * 2)) - 50, 100, 100), items[zoomObject].objectTexture.texture))
                 {
                     zoom = true;
                     zoomObject = i;
