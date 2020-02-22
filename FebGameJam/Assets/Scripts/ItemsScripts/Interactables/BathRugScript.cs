7ff025074f6140a352224b6ada836ba388600aca
ctd2658 <ctd2658@g.rit.edu>
2020-02-22 15:24:24 -0500

    A lot of scene changes and new scripts for items

---
+++ FebGameJam/Assets/Scripts/ItemsScripts/Interactables/BathRugScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathRugScript : InteractableItem
+{
+    [SerializeField] private GameObject placeToPlace;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        placeToPlace.transform.position = new Vector3(placeToPlace.transform.position.x - 4f, placeToPlace.transform.position.y, placeToPlace.transform.position.z);
+
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + 4f, gameObject.transform.position.y, gameObject.transform.position.z);
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
