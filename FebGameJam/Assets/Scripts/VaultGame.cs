eff4c3259c54cf7690bdb0948f1f311fe981459a
ctd2658 <ctd2658@g.rit.edu>
2020-02-22 10:43:02 -0500

    Started setting up the gameplay scene/ polished the vault minigame

--- FebGameJam/Assets/Scripts/VaultGame.cs
+++ FebGameJam/Assets/Scripts/VaultGame.cs
@@ -4,51 +4,132 @@ using UnityEngine;
 
 public class VaultGame : InteractableItem
 {
+    // vault Image to turn
+    public GameObject vaultKnob;
     // int for the vault number
     private int vaultNum;
+    // float for the time between actions
     private float nextActionTime;
+    // float for the time between landing on the correct number
+    private float correctActionTime;
+    // the vault code
     private int[] code;
+    // when you finish one number of the sequence
     private int countDone;
+    // bool for which way you are turning
+    private bool turningRight;
+    // canvas of the game
+    public GameObject screen;
+    // boolean for if in the game already
+    private bool inGame;
 
     // Start is called before the first frame update
     void Start()
     {
         vaultNum = 0;
         nextActionTime = 0.0f;
+        correctActionTime = 0.0f;
         code = new int[3];
         countDone = 0;
+        turningRight = false;
+        inGame = false;
 
         code[0] = Random.Range(0, 360);
         code[1] = Random.Range(0, 360);
         code[2] = Random.Range(0, 360);
+
+        screen.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // User input for the game Q/E
+        UserInput();
+
+        // checking if the user is on the right space of knob
+        CheckKnob();
+
+        // check if they have won
+        CheckWin();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (!inGame)
+        {
+            screen.SetActive(true);
+            inGame = true;
+        }
+    }
+
+    private void UserInput()
+    {
+        // user presses q go right
         if (Input.GetKey(KeyCode.Q))
         {
-            if (Time.time > nextActionTime)
+            nextActionTime += Time.deltaTime;
+            if (nextActionTime >= .2f)
             {
-                nextActionTime+=.1f;
-                if(vaultNum == 361) { vaultNum = 0; }
                 vaultNum++;
-                gameObject.transform.Rotate(new Vector3(0, 0, 1));
+                if (vaultNum == 361) { vaultNum = 0; }
+                vaultKnob.transform.Rotate(new Vector3(0, 0, 1));
+                turningRight = true;
+                nextActionTime %= .2f;
             }
         }
+        // user presses e go left
         else if (Input.GetKey(KeyCode.E))
         {
-            if (Time.time > nextActionTime)
+            nextActionTime += Time.deltaTime;
+            if (nextActionTime >= .2f)
             {
-                nextActionTime+=.1f;
-                if(vaultNum == -1) { vaultNum = 360; }
                 vaultNum--;
-                gameObject.transform.Rotate(new Vector3(0, 0, -1));
+                if (vaultNum == -1) { vaultNum = 360; }
+                vaultKnob.transform.Rotate(new Vector3(0, 0, -1));
+                turningRight = false;
+                nextActionTime %= .2f;
             }
         }
+    }
 
-        if(code[countDone] == vaultNum) { countDone++; }
+    // check to see if they are on the right space
+    private void CheckKnob()
+    {
+        // if user is on the number wait a second if they are still on it while going in the right direction
+        if (code[countDone] == vaultNum && ((turningRight && countDone % 2 == 0) || (!turningRight && countDone % 2 == 1)))
+        {
+            vaultKnob.GetComponent<UnityEngine.UI.Image>().color = Color.red;
+            correctActionTime += Time.deltaTime;
+            if (correctActionTime >= 2f)
+            {
+                countDone++;
+                vaultKnob.GetComponent<UnityEngine.UI.Image>().color = Color.white;
+                correctActionTime %= 2f;
+            }
+        }
+        else
+        {
+            vaultKnob.GetComponent<UnityEngine.UI.Image>().color = Color.white;
+        }
+    }
 
-       
+    // check if the user has won
+    private void CheckWin()
+    {
+        // if countDone is 2 i.e. finished make the game stop and open the vault
+        if (countDone == 3)
+        {
+            inGame = false;
+            Destroy(gameObject);
+        }
+    }
+
+    // Exit button code
+    public void ExitButton()
+    {
+        screen.SetActive(false);
+        inGame = false;
     }
 }
